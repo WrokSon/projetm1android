47bb108f7e7a0640ec02e35868e76d2824ec939d
Williame <williame.massondi@ulb.be>
2023-12-12 22:33:02 +0100

    [ADD] debut langues

--- app/src/main/java/com/example/design/views/Parametre.kt
+++ app/src/main/java/com/example/design/views/Parametre.kt
@@ -2,8 +2,12 @@ package com.example.design.views
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -15,6 +19,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.design.R
 import com.example.model.tools.SaveLocal
 import com.example.viewmodel.ParametreViewModel
+import java.util.Locale
 
 class Parametre : AppCompatActivity() {
 
@@ -57,6 +62,58 @@ class Parametre : AppCompatActivity() {
             viewModel.setResetValue(true)
             Toast.makeText(this,"frero c'est fait",Toast.LENGTH_LONG).show()
         }
+        // Langue
 
+        val languageSpinner : Spinner = findViewById(R.id.spinner)
+
+        // Adapter pour le Spinner
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.languages,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        languageSpinner.adapter = adapter
+
+        // Gérer les sélections du Spinner
+        languageSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // Changer la langue en fonction de la sélection du Spinner
+                changeLanguage(position)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Ne rien faire dans ce cas
+            }
+        }
     }
+
+    private fun changeLanguage(position: Int) {
+        val localeCode = when (position) {
+            0 -> "en" // Anglais
+            1 -> "fr" // Français
+            else -> "fr"
+        }
+
+        val locale = Locale(localeCode)
+        Locale.setDefault(locale)
+
+        val configuration = Configuration(resources.configuration)
+        configuration.setLocale(locale)
+
+        // Mettre à jour la configuration de l'application
+        baseContext.resources.updateConfiguration(
+            configuration,
+            baseContext.resources.displayMetrics
+        )
+
+        // Redémarrer l'activité pour appliquer la nouvelle langue
+        recreate()
+    }
+
 }
